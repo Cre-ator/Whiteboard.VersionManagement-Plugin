736cccc66925dff223dc7df6842181c0df98af51
Stefan Schwarz <s.schwarz@online.de>
2016-09-15 16:17:44 +0200

    security update

--- VersionManagement/pages/version_view_delete.php
+++ VersionManagement/pages/version_view_delete.php
@@ -6,26 +6,30 @@ require_once ( __DIR__ . DIRECTORY_SEPARATOR . '..' . DIRECTORY_SEPARATOR . 'cor
  */
 auth_reauthenticate ();
 
+$pluginWriteAccessLevel = plugin_config_get ( 'access_level' );
+
+$userId = auth_get_current_user_id ();
 $versionId = gpc_get_int ( 'version_id' );
 $version = new vmVersion( $versionId );
+$versionAccessLevel = vmApi::getProjectUserAccessLevel ( $version->getProjectId (), $userId );
 
-/** check if user has level in related project */
-access_ensure_project_level ( config_get ( 'manage_project_threshold' ), $version->getProjectId () );
-
-/** delete without request, if the version is not used anywhere */
-if ( !$version->isVersionIsUsed () )
+if ( ( $versionAccessLevel >= $pluginWriteAccessLevel ) || ( user_is_administrator ( $userId ) ) )
 {
-   /** remove version */
-   $version->triggerDeleteFromDb ();
-}
-else
-{
-   /** ensure that user confirms request to remove the version */
-   helper_ensure_confirmed ( lang_get ( 'version_delete_sure' ) . '<br/>' . lang_get ( 'word_separator' ) .
-      string_display_line ( $version->getVersionName () ), lang_get ( 'delete_version_button' ) );
+   /** delete without request, if the version is not used anywhere */
+   if ( !$version->isVersionIsUsed () )
+   {
+      /** remove version */
+      $version->triggerDeleteFromDb ();
+   }
+   else
+   {
+      /** ensure that user confirms request to remove the version */
+      helper_ensure_confirmed ( lang_get ( 'version_delete_sure' ) . '<br/>' . lang_get ( 'word_separator' ) .
+         string_display_line ( $version->getVersionName () ), lang_get ( 'delete_version_button' ) );
 
-   /** remove version */
-   $version->triggerDeleteFromDb ();
+      /** remove version */
+      $version->triggerDeleteFromDb ();
+   }
 }
 
 /** redirect to view page */
